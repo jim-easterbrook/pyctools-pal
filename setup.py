77762a4107c5ae37e502c2b0f459849d64e99f16
Jim Easterbrook <jim@jim-easterbrook.me.uk>
2016-02-01 16:00:21 +0000

    Update setup.py to match latest pyctools.core

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 #  Pyctools-PAL - PAL coding and decoding with Pyctools.
 #  http://github.com/jim-easterbrook/pyctools-pal
-#  Copyright (C) 2014  Jim Easterbrook  jim@jim-easterbrook.me.uk
+#  Copyright (C) 2014-16  Jim Easterbrook  jim@jim-easterbrook.me.uk
 #
 #  This program is free software: you can redistribute it and/or
 #  modify it under the terms of the GNU General Public License as
@@ -33,7 +33,13 @@ for root, dirs, files in os.walk('src/pyctools'):
         packages.append(package + '.' + name)
 
 # make sure each package is a "namespace package"
-init_text = "__import__('pkg_resources').declare_namespace(__name__)\n"
+init_text = """__import__('pkg_resources').declare_namespace(__name__)
+
+try:
+    from .__doc__ import __doc__
+except ImportError:
+    pass
+"""
 for package in packages:
     path = os.path.join('src', package.replace('.', os.sep), '__init__.py')
     if os.path.exists(path):
