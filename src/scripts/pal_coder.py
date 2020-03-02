82ec1606d5fd72957ba981e4d788e5a4cb81e2de
Jim Easterbrook <jim@jim-easterbrook.me.uk>
2020-03-02 08:35:55 +0000

    Update simple PAL coder/decoder scripts
    Signed-off-by: Jim Easterbrook <jim@jim-easterbrook.me.uk>

--- src/scripts/pal_coder.py
+++ src/scripts/pal_coder.py
@@ -3,6 +3,10 @@
 
 import argparse
 import logging
+import sys
+
+from PyQt5 import QtCore, QtWidgets
+
 from pyctools.core.compound import Compound
 import pyctools.components.io.dumpmetadata
 import pyctools.components.io.videofilereader
@@ -17,29 +21,31 @@ class Network(object):
                  'config': '{}',
                  'pos': (110.0, 60.0)},
     'coder': {   'class': 'pyctools.components.pal.coder.Coder',
-                 'config': "{'rgbyuv': {'matrix': '601', 'outframe_pool_len': "
-                           "5}, 'prefilter': {}, 'modulator': {}, 'matrix': "
-                           "{}, 'assemble': {'func': '((data1 + data2) * "
-                           "pt_float(140.0 / 255.0)) + pt_float(64.0)'}, "
-                           "'postfilter': {'resize': {}, 'fildes': "
-                           "{'frequency': '0.0, 0.307, 0.317, 0.346, 0.356, "
-                           "0.5', 'gain': '     1.0, 1.0,   1.0,   0.0,   "
-                           "0.0,   0.0', 'weight': '   1.0, 1.0,   0.0,   "
-                           "0.0,   1.0,   1.0', 'aperture': 17}}}",
+                 'config': "{'rgbyuv': {'matrix': '601', 'audit': 'Y', "
+                           "'outframe_pool_len': 5}, 'prefilter': {}, "
+                           "'modulator': {}, 'matrix': {}, 'assemble': "
+                           "{'func': '((data1 + data2) * pt_float(140.0 / "
+                           "255.0)) + pt_float(64.0)'}, 'postfilter': "
+                           "{'resize': {}, 'fildes': {'frequency': '0.0, "
+                           "0.307, 0.317, 0.346, 0.356, 0.5', 'gain': '     "
+                           "1.0, 1.0,   1.0,   0.0,   0.0,   0.0', 'weight': "
+                           "'   1.0, 1.0,   0.0,   0.0,   1.0,   1.0', "
+                           "'aperture': 61}}}",
                  'expanded': False,
                  'pos': (-20.0, -50.0)},
     'display': {   'class': 'pyctools.components.qt.qtdisplay.QtDisplay',
-                   'config': "{'framerate': 40}",
+                   'config': '{}',
                    'pos': (110.0, -50.0)},
     'filereader': {   'class': 'pyctools.components.io.videofilereader.VideoFileReader',
                       'config': "{'path': "
-                                "'/home/jim/Videos/test_seqs/mobcal.avi'}",
+                                "'/home/jim/Videos/test_seqs/mobcal.avi', "
+                                "'noaudit': True}",
                       'pos': (-270.0, -50.0)},
     'filewriter': {   'class': 'pyctools.components.io.videofilewriter.VideoFileWriter',
                       'config': "{'path': "
-                                "'/home/jim/Documents/projects/pyctools-pal/coded_pal.avi', "
+                                "'/home/jim/Documents/projects/pyctools/pyctools-pal/coded_pal.avi', "
                                 "'encoder': '-c:v ffv1 -pix_fmt gray16le', "
-                                "'16bit': 1}",
+                                "'16bit': True}",
                       'pos': (110.0, -160.0)},
     'resample': {   'class': 'pyctools.components.pal.common.To4Fsc',
                     'config': "{'resize': {'xup': 461, 'xdown': 351}, "
@@ -61,9 +67,8 @@ class Network(object):
         return Compound(linkages=self.linkages, **comps)
 
 if __name__ == '__main__':
-    from PyQt5 import QtCore, QtWidgets
     QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_X11InitThreads)
-    app = QtWidgets.QApplication([])
+    app = QtWidgets.QApplication(sys.argv)
 
     comp = Network().make()
     cnf = comp.get_config()
--- src/scripts/pal_decoder.py
+++ src/scripts/pal_decoder.py
@@ -3,6 +3,10 @@
 
 import argparse
 import logging
+import sys
+
+from PyQt5 import QtCore, QtWidgets
+
 from pyctools.core.compound import Compound
 import pyctools.components.io.dumpmetadata
 import pyctools.components.io.videofilereader
@@ -18,24 +22,25 @@ class Network(object):
     'decoder': {   'class': 'pyctools.components.pal.decoder.Decoder',
                    'config': "{'setlevel': {'func': '(data - pt_float(64)) * "
                              "pt_float(255.0 / 140.0)'}, 'filterY': {'resize': "
-                             "{}, 'fildes': {'frequency': '0.0, 0.215, 0.22, "
-                             "0.23, 0.25, 0.27, 0.28, 0.285, 0.5', 'gain': "
-                             "'     1.0, 1.0,   0.8,  0.0,  0.0,  0.0,  0.8,  "
-                             "1.0,   1.0', 'weight': '   0.6, 0.6,   0.6,  "
-                             "1.0,  1.0,  1.0,  0.3,  0.3,   0.3', 'aperture': "
-                             "11}}, 'yuvrgb': {'matrix': '601'}, 'matrix': {}, "
+                             "{}, 'fildes': {'frequency': '0.0,  0.215, 0.22, "
+                             "0.23, 0.25, 0.27, 0.28,  0.285, 0.5', 'gain': "
+                             "'1.0,  1.0,   0.8,  0.0,  0.0,  0.0,  0.8,   "
+                             "1.0,   1.0', 'weight': '0.01, 0.01,  0.01, 0.1,  "
+                             "1.0,  0.1,  0.005, 0.005, 0.01', 'aperture': "
+                             "13}}, 'yuvrgb': {'matrix': '601'}, 'matrix': {}, "
                              "'demod': {}, 'filterUV': {'resize': {}, "
                              "'filgen': {'xaperture': 12, 'xcut': 22}}}",
                    'expanded': False,
-                   'pos': (170.0, 150.0)},
+                   'pos': (160.0, 150.0)},
     'display': {   'class': 'pyctools.components.qt.qtdisplay.QtDisplay',
-                   'config': "{'framerate': 50, 'stats': 1}",
+                   'config': "{'stats': True}",
                    'pos': (420.0, 150.0)},
     'filereader': {   'class': 'pyctools.components.io.videofilereader.VideoFileReader',
                       'config': "{'path': "
-                                "'/home/jim/Documents/projects/pyctools-pal/coded_pal.avi', "
-                                "'looping': 'repeat', 'type': 'Y', '16bit': 1}",
-                      'pos': (50.0, 150.0)},
+                                "'/home/jim/Documents/projects/pyctools/pyctools-pal/coded_pal.avi', "
+                                "'looping': 'repeat', 'type': 'Y', '16bit': "
+                                "True, 'noaudit': True, 'zperiod': 4}",
+                      'pos': (30.0, 150.0)},
     'resample': {   'class': 'pyctools.components.pal.common.From4Fsc',
                     'config': "{'resize': {'xup': 351, 'xdown': 461}, "
                               "'filgen': {'xup': 351, 'xdown': 461, "
@@ -45,7 +50,7 @@ class Network(object):
     linkages = \
 {   ('decoder', 'output'): [('resample', 'input')],
     ('filereader', 'output'): [('decoder', 'input')],
-    ('resample', 'output'): [('display', 'input'), ('audit', 'input')]}
+    ('resample', 'output'): [('audit', 'input'), ('display', 'input')]}
 
     def make(self):
         comps = {}
@@ -54,9 +59,8 @@ class Network(object):
         return Compound(linkages=self.linkages, **comps)
 
 if __name__ == '__main__':
-    from PyQt5 import QtCore, QtWidgets
     QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_X11InitThreads)
-    app = QtWidgets.QApplication([])
+    app = QtWidgets.QApplication(sys.argv)
 
     comp = Network().make()
     cnf = comp.get_config()
